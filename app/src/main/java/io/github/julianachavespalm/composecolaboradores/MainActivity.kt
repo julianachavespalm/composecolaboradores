8176d92aa51e80c1fd92dde714e39c3433fe96c5
Juliana <julianachavespalm@gmail.com>
2026-06-27 20:39:27 -0300

    refactor: renomeia repositório e reorganiza estrutura de pacotes da UI

--- app/src/main/java/io/github/julianachavespalm/composecolaboradores/MainActivity.kt
+++ app/src/main/java/io/github/julianachavespalm/composecolaboradores/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import io.github.julianachavespalm.composecolaboradores.data.repository.ColaboradorRepository
+import io.github.julianachavespalm.composecolaboradores.data.repository.InMemoryColaboradorRepository
 import io.github.julianachavespalm.composecolaboradores.domain.usecase.GetColaboradoresUseCase
 import io.github.julianachavespalm.composecolaboradores.domain.usecase.RemoverColaboradorUseCase
 import io.github.julianachavespalm.composecolaboradores.domain.usecase.SalvarColaboradorUseCase
 import io.github.julianachavespalm.composecolaboradores.domain.usecase.ValidarEmailUseCase
-import io.github.julianachavespalm.composecolaboradores.ui.ColaboradorViewModel
-import io.github.julianachavespalm.composecolaboradores.ui.GerenciadorColaboradoresScreen
+import io.github.julianachavespalm.composecolaboradores.ui.gerenciador.ColaboradorViewModel
+import io.github.julianachavespalm.composecolaboradores.ui.gerenciador.GerenciadorColaboradoresScreen
 import io.github.julianachavespalm.composecolaboradores.ui.theme.ComposeColaboradoresTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
 
-        val repository = ColaboradorRepository()
+        val repository = InMemoryColaboradorRepository()
         val viewModel = ColaboradorViewModel(
             GetColaboradoresUseCase(repository),
             SalvarColaboradorUseCase(repository),
